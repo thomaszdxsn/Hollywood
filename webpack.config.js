4d85e2dab875a0714edd4b03edee4a58d416af64
yang zhou <bnm965321@gmail.com>
2018-09-23 18:14:11 +0800

    加入antd

--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,18 @@ module.exports = {
                 use: {
                     loader: "babel-loader"
                 }
+            },
+            {
+                test: /\.css$/,
+                use: [ 'style-loader', 'css-loader' ]
+            },
+            {
+                test: /\.(png|jpg|gif|svg|eot|ttf|woff|woff2)$/,
+                loader: 'url-loader',
+                options: {
+                  limit: 10000
+                }
             }
         ]
     }
-}
+};
